8258930aa60ecdd6b25cdc760e0f54652ec90d0e
TruongNhatMinh_B2014584 <buaminh69@gmail.com>
2023-11-06 23:03:42 +0700

    10%

---
+++ src/Userauthentication.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Userauthentication {
+    public boolean authenticateUser(String username, String password) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/Hospital";
+        String dbUser = "root";
+        String dbPassword = "root";
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
+
+            String sqlQuery = "SELECT * FROM account WHERE username = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.setString(1, username);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            // Nếu có bất kỳ dòng nào khớp với username
+            if (resultSet.next()) {
+                String storedPassword = resultSet.getString("password");
+                // So sánh password lấy từ cơ sở dữ liệu với password được cung cấp
+                if (password.equals(storedPassword)) {
+                    return true; // Đăng nhập thành công
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false; // Nếu không có kết quả khớp hoặc xảy ra lỗi, từ chối truy cập
+    }
+}
